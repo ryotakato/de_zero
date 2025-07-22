30c83dc9e5bf6a5637668643651aea0de8bdf2f9
ryotakato <ryotakato4@gmail.com>
2025-07-22 19:13:50 +1200

    add chap04 training neural network

---
+++ chap04_train_neuralnet/train_neuralnet.py
@@ -0,0 +1,73 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+
+# parameter
+#iters_num = 10000
+iters_num = 1000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+# iterate number per each epoch (epoch = train_size / batch_size)
+iter_per_epoch = max(train_size / batch_size, 1)
+
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    print(f'Iter Num: {i}')
+    # mini batch
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # calculate gradient
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    # update parameter
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # record learning progress
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+
+    # calculate accuracy per epoch
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+
+# show loss
+#x = np.arange(len(train_loss_list))
+#plt.plot(x, train_loss_list)
+#plt.ylim(0, 4)
+#plt.show()
+
+# show accuracy
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc="lower right")
+plt.show()
+
+
+
---
+++ common/functions.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int64)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def identity_function(x):
+    return x
+
+def softmax(x):
+    x = x - np.max(x, axis=-1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    # if teacher data is one-hot-vector, convert to currect labels
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    delta = 1e-7
+    return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
+
